1e66bc5b0c5896bb8d312a57fc0c804d21f8ee8c
Tolik9924 <sakurasoft9924@gmail.com>
2025-09-26 10:06:20 +0300

    refactor movie controller

--- src/controllers/movieController.ts
+++ src/controllers/movieController.ts
@@ -1,28 +1,9 @@
-import axios from "axios";
 import { Request, Response } from "express";
-import pool from "../db";
 import UserService from "../services/userService";
 import MovieService from "../services/movieService";
 
 const userService = new UserService();
 const movieService = new MovieService();
-
-const getUser = async (username: string) => {
-  const userResult = await pool.query("SELECT * FROM Users WHERE username = $1", [username]);
-  let user;
-
-  if (!userResult.rows.length) {
-    const newUser = await pool.query("INSERT INTO users(username) VALUES($1) RETURNING *", [
-      username,
-    ]);
-
-    user = newUser.rows[0];
-  } else {
-    user = userResult.rows[0];
-  }
-
-  return user;
-};
 
 export const createMovie = async (req: Request, res: Response) => {
   try {
@@ -85,11 +66,11 @@ export const showAllFavorites = async (req: Request, res: Response) => {
     const { title } = req.query;
     const { username } = req.body;
 
-    const user = await userService.getUser(username);
-
     if (!title) {
       return res.status(400).json({ error: "Title is required" });
     }
+
+    const user = await userService.getUser(username);
     const favorites = await movieService.getFavorites(title as string, user.id);
 
     res.json(favorites);
@@ -104,36 +85,10 @@ export const showMovieInfo = async (req: Request, res: Response) => {
     const { imdbID } = req.params;
     const { username } = req.body;
 
-    const user = await getUser(username);
+    const user = await userService.getUser(username);
+    const movieInfo = await movieService.getMovieInfo(imdbID, user.id);
 
-    const { data: omdbData } = await axios.get("http://www.omdbapi.com/", {
-      params: {
-        apikey: process.env.OMDB_API_KEY,
-        i: imdbID,
-      },
-    });
-
-    const { rows } = await pool.query(`SELECT * FROM movies WHERE "imdbID" = $1 AND user_id = $2`, [
-      imdbID,
-      user.id,
-    ]);
-
-    if (rows.length > 0) {
-      const dbMovie = rows[0];
-      const addData = typeof omdbData === "string" ? { Response: "False" } : omdbData;
-      const merged = {
-        ...addData,
-        Title: dbMovie.Title ?? omdbData.Title,
-        Year: dbMovie.Year ?? omdbData.Year,
-        Runtime: dbMovie.Runtime ?? omdbData.Runtime,
-        Genre: dbMovie.Genre ?? omdbData.Genre,
-        Director: dbMovie.Director ?? omdbData.Director,
-        imdbID: dbMovie.imdbID ?? omdbData.imdbID,
-      };
-      return res.json(merged);
-    }
-
-    res.json(omdbData);
+    res.json(movieInfo);
   } catch (err) {
     console.error(err);
     res.status(500).json({ error: "Database error" });
